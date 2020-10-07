830f04e530388d5f5dd07fccf6719109677744a3
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-07 21:14:41 +0900

    02. 기초 - 01. Print 함수의 이해(1)

---
+++ section02-1.py
@@ -0,0 +1,22 @@
+# Section02-1
+# 파이썬 기초 코딩
+# Print 구문의 이해
+
+# 기본출력
+print('Hello Python!')
+print("Hello Python!")
+print("""Hello Python!""")
+print('''Hello Python!''')
+
+print()
+
+# Separator 옵션 사용
+print('T', 'E', 'S', 'T')
+print('T', 'E', 'S', 'T', sep='')
+print('2019', '02', '19', sep='-')
+print('niceman', 'google.com', sep='@')
+
+# end 옵션 사용
+print('Welcome To', end=' ')
+print('the black parade', end=' ')
+print('piano notes')
