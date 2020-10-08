027810f0e5b6f58d1d431465b472d073955f9c7a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-09 08:07:02 +0900

    02. 기초 - 05. 몸 풀기 코딩 해보기(2)

--- Section02-2.py
+++ Section02-2.py
@@ -18,3 +18,35 @@ myName = 'Goodboy'
 # 조건문
 if myName == "Goodboy":
     print("Ok")
+
+# 반복문
+for i in range(1, 10):
+    for j in range(1, 10):
+        print("%d * %d = " % (i, j), i*j)
+
+# 변수 선언
+name = "ryu"
+
+# 출력
+print(name)
+
+# 함수 선언
+def myFunc():
+    print("안녕하세요. 반갑습니다.")
+
+myFunc()
+
+def greeting():
+    print("Hello!")
+
+greeting()
+
+# 클래스
+class Cookie:
+    pass
+
+# 객체 생성
+cookie = Cookie()
+
+print(id(cookie))
+print(dir(cookie))
