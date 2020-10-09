73c1a5b57947480c5fba9ec75bcdad62460f2cfc
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-09 22:48:29 +0900

    04. 자료형 - 01. 데이터타입, 숫자형 및 연산자(1)

---
+++ section04-1.py
@@ -0,0 +1,35 @@
+# 데이터 타입
+
+v_str1 = "Niceman"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name" : "Kim",
+    "age" : 25
+}
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7, 8, 9}
+
+print(type(v_tuple))
+print(type(v_set))
+print(type(v_float))
+
+i1 = 39
+i2 = 939
+big_int1 = 999999999999999999999999999999999999999999999999
+big_int2 = 777777777777777777777777777777777777777777777777
+f1 = 1.234
+f2 = 3.784
+f3 = .5
+f4 = 10.
+
+print(i1 * i2)
+print(big_int1 * big_int2)
+print(f1 ** f2)
+print(f3 + i2)
+
+result = f3 + i2
+print(result, type(result))
