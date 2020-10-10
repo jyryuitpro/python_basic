4e9a694350aa4632fe0216d05e1ed6cbc214e756
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-10 23:48:49 +0900

    04. 자료형 - 09. 딕셔너리, 집합(2)

--- section04-4.py
+++ section04-4.py
@@ -40,3 +40,42 @@ print(a.items())
 print(list(a.items()))
 print(1 in b)
 print("name" in a)
+
+# 집합(Sets) (순서x, 중복x)
+a = set()
+b = set([1, 2, 3, 4])
+c = set([1, 4, 4, 6, 6])
+
+print(type(a))
+print(c)
+
+t = tuple(b)
+print(t)
+l = list(b)
+print(l)
+
+print()
+print()
+
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+
+print(s1.intersection(s2))
+print(s1 & s2)
+
+print(s1 | s2)
+print(s1.union(s2))
+
+print(s1 - s2)
+print(s1.difference(s2))
+
+# 추가 & 제거
+s3 = set([7, 8, 10, 15])
+s3.add(18)
+s3.add(7)
+
+print(s3)
+s3.remove(15)
+print(s3)
+
+print(type(s3))
