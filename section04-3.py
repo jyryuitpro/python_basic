9bced39ce3739b4bb80826d52d176395bf191aa1
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-10 10:16:42 +0900

    04. 자료형 - 07. 리스트, 튜플(3)

--- section04-3.py
+++ section04-3.py
@@ -67,4 +67,35 @@ y.extend(ex)
 print(y)
 
 # 삭제 : del, remove, pop
+print()
+print()
+print()
+
+# 튜플 (순서○, 중복○, 수정x, 삭제x)
+
+a = ()
+b = (1,)
+c = (1, 2, 3, 4)
+d = (10, 100, ('a', 'b', 'c'))
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2:])
+print(d[2][0:2])
+
+print(c + d)
+print(c * 3)
+print()
+print()
+
+# 튜플 함수
+z = (5, 2, 1, 3, 1)
+print(z)
+print(3 in z)
+print(z.index(5))
+print(z.count(1))
+
+
 
