5ac16a2563e473ae83021fde3bb0dc5dacb902de
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-12 23:18:21 +0900

    05. 흐름 제어 - 04. 반복문(2)

--- section05-2.py
+++ section05-2.py
@@ -30,3 +30,47 @@ print("1 ~ 100 : ", sum(range(1, 101)))
 print("1 ~ 100 : ", sum(range(1, 101, 2)))
 for num in range(1, 101, 2):
     print(num)
+
+# 시퀀스(순서가 있는)자료형 반복
+# 문자열, 리스트, 튜플, 집합, 사전
+# iterable : range, reversed, enumerate, filter, map, zip
+
+names = ["Kim", "Park", "Cho", "Choi", "You",]
+
+for name in names:
+    print(name)
+
+word = "dreams"
+
+for s in word:
+    print(s)
+
+my_info = {
+    "name": "Kim",
+    "age": 33,
+    "city": "Seoul",
+}
+
+# 기본 값은 키
+for key in my_info:
+    print(key)
+
+# 값
+for key in my_info.values():
+    print(key)
+
+# 키
+for key in my_info.keys():
+    print(key)
+
+# 키 and 값
+for k, v in my_info.items():
+    print(k, v)
+
+name = "KennRy"
+
+for n in name:
+    if n.isupper():
+        print(n.lower())
+    else:
+        print(n.upper())
