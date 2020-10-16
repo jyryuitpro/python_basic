dcaaa513dbd70a13876a04a471d9b9044d3b5847
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-16 21:06:09 +0900

    06. 함수 - 02. 함수 및 람다(lambda)(2)

--- section06.py
+++ section06.py
@@ -45,3 +45,48 @@ def func_mul2(x):
 
 lt = func_mul2(100)
 print(lt)
+
+# 예제4
+# *args, *kwargs
+
+def args_func(*args):
+    # print(type(args))
+    # for t in args:
+    #     print(t)
+    for i, v in enumerate(args):
+        print(i, v)
+
+    # for i, v in enumerate(range(10)):
+    #     print(i, v)
+
+
+args_func("kim")
+args_func("kim", "Park")
+args_func("kim", "Park", "Lee")
+
+# kwargs
+def kwargs_func(**kwargs):
+    print(kwargs)
+
+    for k, v in kwargs.items():
+        print(k, v)
+
+kwargs_func(name1="Kim")
+kwargs_func(name1="Kim", name2="Park", name3="Lee")
+
+# 전체 혼합
+def example_mul(arg1, arg2, *args, **kwargs):
+    print(arg1, arg2, args, kwargs)
+
+example_mul(10, 20)
+example_mul(10, 20, "park", "kim", age1=24, age2=35)
+
+# 중첩함수(클로저)
+# 데코레이터
+def nested_func(num):
+    def func_in_func(num):
+        print(">>>", num)
+    print("in func")
+    func_in_func(num + 10000)
+
+nested_func(10000)
