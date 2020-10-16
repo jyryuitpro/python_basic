cbb44c5082e21e37625cc92901946c899412a55f
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-16 21:20:24 +0900

    06. 함수 - 03. 함수 및 람다(lambda)(3)

--- section06.py
+++ section06.py
@@ -81,6 +81,7 @@ def example_mul(arg1, arg2, *args, **kwargs):
 example_mul(10, 20)
 example_mul(10, 20, "park", "kim", age1=24, age2=35)
 
+# 예제5
 # 중첩함수(클로저)
 # 데코레이터
 def nested_func(num):
@@ -90,3 +91,47 @@ def nested_func(num):
     func_in_func(num + 10000)
 
 nested_func(10000)
+
+# 예제6(hint)
+def func_mul3(x : int) -> list:
+    y1 = x*100
+    y2 = x*200
+    y3 = x*300
+    return [y1, y2, y3]
+    # return (y1, y2, y3)
+
+print(func_mul3(5.0))
+
+# 람다식 예제
+# 람다식 : 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스(메모리) 할당
+# 람다는 즉시 실행(Heap 초기화) -> 메모리 초기화
+
+# 일반적 함수 -> 변수 할당
+def mul_10(num : int) -> int:
+    return num * 10
+
+var_func = mul_10
+print(var_func)
+print(type(var_func))
+
+print(var_func(10))
+
+lambda_mul_10 = lambda num: num * 10
+
+print(">>>", lambda_mul_10(10))
+
+def func_final(x, y, func):
+    print(x * y * func(10))
+
+func_final(10, 10, lambda_mul_10)
+
+func_final(10, 10, lambda x : x * 1000)
+
+
+
+
+
+
+
+
