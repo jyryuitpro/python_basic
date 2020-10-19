824914b174389f60c1111d7ad6d76c0dda327210
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-19 23:27:55 +0900

    08. 모듈, 패키지 - 01. 모듈, 패키지(1)

---
+++ pkg/calculations.py
@@ -0,0 +1,8 @@
+def add(l, r):
+    return 1 + r
+
+def add(l, r):
+    return 1 * r
+
+def add(l, r):
+    return 1 / r
---
+++ pkg/prints.py
@@ -0,0 +1,5 @@
+def prt1():
+    print("I'm Niceboy!")
+
+def prt2():
+    print("I'm Goodboy!")
