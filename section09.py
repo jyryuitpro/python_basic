f85fc5893418254d3b2885d4cde2417b1326f2c8
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-20 23:58:30 +0900

    09. 파일 - 02. 파일 읽기, 파일 쓰기(2)

--- section09.py
+++ section09.py
@@ -51,3 +51,53 @@ with open("./resource/review.txt", "r") as f:
         print(line, end='#### ')
         line = f.readline()
 
+print("------------------------------")
+print("------------------------------")
+
+# 예제6
+with open("./resource/review.txt", "r") as f:
+    contents = f.readlines()
+    print(contents)
+    for c in contents:
+        print(c, end=' ****** ')
+
+print("------------------------------")
+print("------------------------------")
+
+# 예제7
+score = []
+with open("./resource/score.txt", "r") as f:
+    for line in f:
+        score.append(int(line))
+    print(score)
+
+print("Average : {:6.3}".format(sum(score)/len(score)))
+
+# 파일 쓰기
+
+# 예제1
+with open("./resource/test1.txt", "w") as f:
+    f.write("Niceman!\n")
+
+# 예제2
+with open("./resource/test1.txt", "a") as f:
+    f.write("Goodman!\n")
+
+# 예제3
+from random import randint
+
+with open("./resource/test2.txt", "w") as f:
+    for cnt in range(6):
+        f.write(str(randint(1, 50)))
+        f.write("\n")
+
+# 예제4
+# writelines : 리스트 -> 파일로 저장
+with open("./resource/test3.txt", "w") as f:
+    list = ["Kim\n", "Park\n", "Cho\n"]
+    f.writelines(list)
+
+# 예제5
+with open("./resource/test4.txt", "w") as f:
+    print("Test Contents1!", file=f)
+    print("Test Contents2!", file=f)
